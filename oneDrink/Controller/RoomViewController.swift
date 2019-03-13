e0a3edcfcf98617cf76e4ac1f46e4b20dacac853
eckran <eckan@live.Fr>
2019-03-13 22:44:53 +0100

    get firebase

--- oneDrink/Controller/RoomViewController.swift
+++ oneDrink/Controller/RoomViewController.swift
@@ -1,40 +1,54 @@
-//
-//  RoomViewController.swift
-//  oneDrink
-//
-//  Created by code on 11/03/2019.
-//  Copyright © 2019 code. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 class RoomViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var ref : DatabaseReference!
+    var userId : String = ""
+    var userTab = [String: Any?]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chargeAllUser()
+        
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 20
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionViewCell
-        
-        let url = URL(string: "https://www.wikichat.fr/wp-content/uploads/sites/2/comment-soigner-une-plaie-dun-chat.jpg")
-        let dataImage = try? Data(contentsOf: url!)
-        
-        cell.userName.text = "romain"
-        cell.userAlcohol.text = "Rhum"
-        cell.profileImage.image = UIImage(data: dataImage!)
+
+        ref = Database.database().reference()
+        ref.child("users").observe(.value, with: { snapshot in
+            
+            for userId in snapshot.children.allObjects as! [DataSnapshot]{
+                let user = userId.value as? [String: AnyObject]
+                let userName = user?["firstname"]
+                let userAlcohol = user?["favoriteAlcohol"]
+                let userImgUrl = user?["imageURL"]
+                
+                let url = URL(string: userImgUrl as! String)
+                let dataImage = try? Data(contentsOf: url!)
+                cell.userName.text = userName as! String
+                cell.userAlcohol.text = userAlcohol as! String
+                cell.profileImage.image = UIImage(data: dataImage!)
+                
+                print(userName!)
+                
+            }
+        })
         return cell;
+        
     }
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func chargeAllUser() {
+        
     }
-    
-
 }
